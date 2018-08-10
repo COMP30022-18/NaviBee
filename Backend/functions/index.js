732c4b1648883b12223c51545777970f31d9c30b
CaviarChen <yzj@vlabpro.com>
2018-08-10 22:39:27 +1000

    add initNewUser

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,8 +1,23 @@
 const functions = require('firebase-functions');
+const admin = require('firebase-admin');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp(functions.config().firebase);
+var db = admin.firestore();
+
+
+exports.initNewUser = functions.auth.user().onCreate(event => {
+    // use event instead of event.data
+    const uid = event.uid;
+
+    // write init data for a new user
+    let data = {
+        name: event.displayName,
+        email: event.email,
+        photoURL: event.photoURL,
+        contacts: []
+    };
+
+    let userRef = db.collection('users').doc(uid); // user doc reference
+    userRef.set(data);
+
+});
