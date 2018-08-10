1a7a3857d3ba880b9454f9e1b12366c1f12c43fa
CaviarChen <yzj@vlabpro.com>
2018-08-10 17:44:52 +1000

    testing list friends

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/FriendActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/FriendActivity.java
@@ -4,12 +4,36 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 import android.widget.TextView;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class FriendActivity extends AppCompatActivity {
 
-    private TextView mTextMessage;
+    private FirebaseFirestore db;
+    private String userId;
+
+    ArrayList<String> contactList = new ArrayList<String>();
+    ArrayAdapter<String> contactListAdapter;
+
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
@@ -18,13 +42,13 @@ public class FriendActivity extends AppCompatActivity {
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
                 case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+//                    mTextMessage.setText(R.string.title_home);
                     return true;
                 case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+//                    mTextMessage.setText(R.string.title_dashboard);
                     return true;
                 case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+//                    mTextMessage.setText(R.string.title_notifications);
                     return true;
             }
             return false;
@@ -36,9 +60,58 @@ public class FriendActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_friend);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
+//        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        db = FirebaseFirestore.getInstance();
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+
+        contactListAdapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_list_item_1,
+                contactList);
+
+        ListView listView = (ListView) findViewById(R.id.contactListView);
+        listView.setAdapter(contactListAdapter);
+
+        loadContactList();
     }
+
+    private void loadContactList() {
+        DocumentReference docRef = db.collection("users").document(userId);
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        ArrayList<String> contacts = (ArrayList<String>)document.get("contacts");
+
+
+                        contactList.clear();
+                        contactList.addAll(contacts);
+                        contactListAdapter.notifyDataSetChanged();
+
+
+
+                        Log.d("Firestore", "test");
+
+
+                    } else {
+                        Log.d("Firestore", "data not exists");
+                    }
+                } else {
+                    Log.d("Firestore", "get failed with ", task.getException());
+                }
+            }
+        });
+
+
+
+
+    }
+
+
 
 }
