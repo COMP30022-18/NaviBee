3c5fc3bcb60c88264b2ddedc867a55a94382d9c4
CaviarChen <yzj@vlabpro.com>
2018-08-11 20:41:09 +1000

    init ts project

--- Backend/functions/helper/db.js
+++
@@ -1,6 +0,0 @@
-const functions = require('firebase-functions');
-const admin = require('firebase-admin');
-
-admin.initializeApp(functions.config().firebase);
-
-exports = admin.firestore();
--- Backend/functions/index.js
+++
@@ -1,7 +0,0 @@
-const user = require('./main/user');
-const social = require('./main/social');
-
-
-
-module.exports = Object.assign(module.exports, user);
-module.exports = Object.assign(module.exports, social);
