3a743a896474b1907fa8ad4269114c28d79dd290
CaviarChen <yzj@vlabpro.com>
2018-08-11 20:28:08 +1000

    add getFriendList (not working since function only supports es5)

--- Backend/functions/index.js
+++ Backend/functions/index.js
@@ -1,6 +1,7 @@
 const user = require('./main/user');
-
+const social = require('./main/social');
 
 
 
 module.exports = Object.assign(module.exports, user);
+module.exports = Object.assign(module.exports, social);
