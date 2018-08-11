b672deb80aa2a0e172cb7a6642a1ee604652cd5c
Mars Xue <marsxue0221@gmail.com>
2018-08-11 22:10:00 +1000

    Add SOS activity

---
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/SosActivity.java
@@ -0,0 +1,35 @@
+package au.edu.unimelb.eng.navibee;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.net.Uri;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class SosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sos);
+
+        Button callButton = (Button) findViewById(R.id.callButton);
+        final EditText phoneNumber = (EditText) findViewById(R.id.phoneNumber);
+
+        callButton.setOnClickListener(new View.OnClickListener() {
+            // Check permission function does not work...
+            @SuppressLint("MissingPermission")
+            @Override
+            public void onClick(View view) {
+                Intent callIntent = new Intent(Intent.ACTION_CALL);
+                callIntent.setData(Uri.parse("tel:" + phoneNumber.getText().toString()));
+                callIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+
+                startActivity(callIntent);
+            }
+        });
+    }
+}
