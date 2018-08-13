24f8d8ced2b3795af244973a646a133c9a08162d
CaviarChen <yzj@vlabpro.com>
2018-08-13 22:11:20 +1000

    Add ConversationManager & Conversation

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/MainActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/MainActivity.java
@@ -1,31 +1,18 @@
 package au.edu.unimelb.eng.navibee;
 
 import android.content.Intent;
-import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
 import com.google.android.gms.auth.api.signin.GoogleSignIn;
 import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
-import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
-import com.google.firebase.firestore.DocumentSnapshot;
-import com.google.firebase.firestore.FirebaseFirestore;
-
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
 
 import au.edu.unimelb.eng.navibee.Social.FriendManager;
-import au.edu.unimelb.eng.navibee.Social.MessageManager;
+import au.edu.unimelb.eng.navibee.Social.ConversationManager;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
@@ -51,7 +38,7 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         findViewById(R.id.landing_social_btn).setOnClickListener(this);
 
         FriendManager.init();
-//        MessageManager.init();
+        ConversationManager.init();
 //        initUserData();
     }
 
---
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/Social/Conversation.java
@@ -0,0 +1,94 @@
+package au.edu.unimelb.eng.navibee.Social;
+
+import android.support.annotation.Nullable;
+import android.util.Log;
+
+import com.google.firebase.firestore.DocumentChange;
+import com.google.firebase.firestore.EventListener;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.FirebaseFirestoreException;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+import java.util.Date;
+
+public class Conversation {
+    private final String conversationId;
+    private final String uid;
+    FirebaseFirestore db;
+    private ArrayList<Message> messages = new ArrayList<>();
+
+    private static final String TAG = "conv";
+
+    public Conversation(String id, String uid) {
+        conversationId = id;
+        this.uid = uid;
+        db = FirebaseFirestore.getInstance();
+        listen();
+    }
+
+    private void listen() {
+        db.collection("conversations").document(conversationId)
+                .collection("messages").orderBy("time")
+                .addSnapshotListener(new EventListener<QuerySnapshot>() {
+                    @Override
+                    public void onEvent(@Nullable QuerySnapshot snapshots,
+                                        @Nullable FirebaseFirestoreException e) {
+                        if (e != null) {
+                            Log.w(TAG, "listen:error", e);
+                            return;
+                        }
+
+                        for (DocumentChange dc : snapshots.getDocumentChanges()) {
+                            switch (dc.getType()) {
+                                case ADDED:
+                                    // new message
+                                    messages.add(dc.getDocument().toObject(Message.class));
+                                    break;
+                                case MODIFIED:
+                                    //
+                                    break;
+                                case REMOVED:
+                                    //
+                                    break;
+                            }
+                        }
+                    }
+                });
+    }
+
+
+
+    public static class Message{
+        private String data, type, sender;
+        private Date time;
+
+        public Message() {
+
+        }
+
+        public Message(String data, String sender, Date time, String type) {
+            this.data = data;
+            this.sender = sender;
+            this.time = time;
+            this.type = type;
+        }
+
+        public Date getTime() {
+            return time;
+        }
+
+        public String getData() {
+            return data;
+        }
+
+        public String getSender() {
+            return sender;
+        }
+
+        public String getType() {
+            return type;
+        }
+    }
+
+}
