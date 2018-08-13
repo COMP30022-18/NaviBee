151f4212d6fc340662787f4405ea467e027c6720
CaviarChen <yzj@vlabpro.com>
2018-08-14 00:30:40 +1000

    add addFriend& remove getFriendList in backend

--- Backend/functions/src/main/social.ts
+++ Backend/functions/src/main/social.ts
@@ -1,21 +1,39 @@
 import * as functions from 'firebase-functions'
 import { db } from './../helper/db';
 
-export const getFriendList = functions.https.onCall(
+export const addFriend = functions.https.onCall(
     async (data, context) => {
-    const uid = context.auth.uid;
-    const usersRef = db.collection('users');
-    let queryRef = usersRef.where('contacts.'+uid, '==', true);
-    const snapshot = await queryRef.get();
-    let list = [];
-    snapshot.forEach(doc => {
-        list.push({
-            id: doc.id,
-            name: doc.data().name,
-            email: doc.data().email,
-            photoURL: doc.data().photoURL
-        });
-    });
+        const uid = context.auth.uid;
 
-    return {list: list};
-});
+        if (uid==null) {
+            return {code:-1, msg:"need login"};
+        }
+
+        const targetUid = data.targetUid;
+
+        let doc = await db.collection('users').doc(targetUid).get();
+        if (!doc.exists) {
+            return {code:-1, msg:"target user not exists"};
+        }
+
+        if (doc.data().contacts[uid]) {
+            return {code:-1, msg:"have been friends already"};
+        }
+
+        // set contacts
+        let up = {};
+        up['contacts.'+uid] = true;
+        db.collection('users').doc(targetUid).update(up);
+        up = {};
+        up["contacts."+targetUid] = true;
+        db.collection('users').doc(uid).update(up);
+
+        // set up conversations
+        let conv = {};
+        conv['users'] = {};
+        conv['users'][uid] = true;
+        conv['users'][targetUid] = true;
+        db.collection('conversations').add(conv);
+
+        return {code: 0};
+    });
