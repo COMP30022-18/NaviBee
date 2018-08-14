835ff4eb76be5440055968198dfd815784e57b5b
Shijie Liu <s.liu96@student.unimelb.edu.au>
2018-08-14 17:19:14 +1000

    Establish connection with database; Optimize EventDetail

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/EventActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/EventActivity.java
@@ -19,14 +19,12 @@ import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -39,21 +37,26 @@ import static java.util.stream.IntStream.range;
 
 public class EventActivity extends AppCompatActivity {
 
-    public final static String USER_ID = "au.edu.unimelb.eng.navibee.USERID";
+    public final static String EVENT_ID = "au.edu.unimelb.eng.navibee.EVENTID";
     private FirebaseFirestore db;
     private String userId;
 
-    private List<Map<String, Object>> eventList = new ArrayList<>();
-    private List<Map<String, Object>> eventTag = new ArrayList<>();
+    private List<EventItem> eventList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.acitivity_event);
 
-        // load data
+        userId = FirebaseAuth.getInstance().getCurrentUser().getUid();
+        db = FirebaseFirestore.getInstance();
+
+        // load EventList
         loadEventList();
 
+    }
+
+    private void finalizeEventList(){
         // create Adapter and bind with eventList
         ListView eventListView = findViewById(R.id.event_list_view);
         EventListAdapter adapter = new EventListAdapter();
@@ -64,28 +67,36 @@ public class EventActivity extends AppCompatActivity {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int pos, long l) {
                 //using switch case, to check the condition.
-                Intent intent = new Intent(EventActivity.this, EventDetialActivity.class);
-                intent.putExtra(USER_ID, "userId");
+                Intent intent = new Intent(EventActivity.this, EventDetailActivity.class);
+                intent.putExtra(EVENT_ID, eventList.get(pos).getEventId());
                 startActivity(intent);
             }
         });
-
     }
 
     private void loadEventList() {
-        Map<String, Object> eventListItem = new HashMap<>();
-        eventListItem.put("Name", "EventName");
-        eventListItem.put("Summary", "summary");
-        eventListItem.put("ImageId", R.mipmap.ic_launcher);
 
-        Map<String, Object> eventTagItem = new HashMap<>();
-        eventTagItem.put("Name", "Event Tag");
-        eventTag.add(eventTagItem);
+        EventItem ForYouTag = new EventItem("FOR YOU", null, null);
+        ForYouTag.setTag(true);
+        eventList.add(ForYouTag);
 
-        eventList.add(eventTagItem);
-        for(int i = 0;i < 10;i++) {
-            eventList.add(eventListItem);
-        }
+        db.collection("events").whereEqualTo("users." + userId, true).get()
+                .addOnCompleteListener(new OnCompleteListener<QuerySnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<QuerySnapshot> task) {
+                        if (task.isSuccessful()) {
+                            for (QueryDocumentSnapshot document : task.getResult()) {
+                                EventItem eventItem = document.toObject(EventItem.class);
+                                eventItem.setEventId(document.getId());
+                                eventList.add(eventItem);
+                            }
+                        } else {
+                           // fail to pull data
+                        }
+
+                        finalizeEventList();
+                    }
+                });
     }
 
     private class EventListAdapter extends BaseAdapter {
@@ -107,7 +118,7 @@ public class EventActivity extends AppCompatActivity {
         @Override
         public boolean isEnabled(int position) {
             // TODO Auto-generated method stub
-            if(eventTag.contains(getItem(position))){
+            if(((EventItem)getItem(position)).isTag()){
                 return false;
             }
             return super.isEnabled(position);
@@ -116,62 +127,74 @@ public class EventActivity extends AppCompatActivity {
         public View getView(int position, View convertView, ViewGroup parent) {
             // TODO Auto-generated method stub
             View view=convertView;
-            if(eventTag.contains(getItem(position))){
-                view= LayoutInflater.from(getApplicationContext()).inflate(R.layout.event_tag, null);
+            if(((EventItem)getItem(position)).isTag()){
+                view = LayoutInflater.from(getApplicationContext()).inflate(R.layout.event_tag, null);
 
                 TextView tagView = (TextView) view.findViewById(R.id.event_list_item_tag);
-                tagView.setText((String) eventList.get(position).get("Name"));
+                tagView.setText((String) eventList.get(position).getName());
 
             }else{
                 view=LayoutInflater.from(getApplicationContext()).inflate(R.layout.event_item, null);
 
                 TextView name = (TextView) view.findViewById(R.id.event_name);
-                name.setText((String) eventList.get(position).get("Name"));
+                name.setText((String) eventList.get(position).getName());
 
                 TextView summary = (TextView) view.findViewById(R.id.event_summary);
-                summary.setText((String) eventList.get(position).get("Summary"));
+                summary.setText((String) eventList.get(position).getSummary());
 
                 ImageView image = (ImageView) view.findViewById(R.id.event_image);
-                image.setImageResource((Integer) eventList.get(position).get("ImageId"));
+                image.setImageResource(R.mipmap.ic_launcher);
 
             }
-
             return view;
         }
 
     }
 
+    public static class EventItem {
 
+        private String eventId;
+        private String name;
+        private String summary;
+        private Map<String, Boolean> users;
+        private Boolean isTag = false;
 
+        public EventItem(){}
 
-//    private void loadContactList() {
-//
-//        DocumentReference docRef = db.collection("users").document(userId);
-//        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
-//            @Override
-//            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
-//                if (task.isSuccessful()) {
-//                    DocumentSnapshot document = task.getResult();
-//                    if (document.exists()) {
-//                        ArrayList<String> contacts = (ArrayList<String>)document.get("contacts");
-//
-//
-//                        contactList.clear();
-//                        contactList.addAll(contacts);
-//                        contactListAdapter.notifyDataSetChanged();
-//
-//
-//
-//                        Log.d("Firestore", "test");
-//
-//
-//                    } else {
-//                        Log.d("Firestore", "data not exists");
-//                    }
-//                } else {
-//                    Log.d("Firestore", "get failed with ", task.getException());
-//                }
-//            }
-//        });
-//    }
+        public EventItem(String name, String summary, Map<String, Boolean> users){
+            this.name = name;
+            this.summary = summary;
+            this.users = users;
+            this.isTag = isTag;
+        }
+
+        public String getName(){
+            return name;
+        }
+
+        public String getSummary(){
+            return summary;
+        }
+
+        public Map<String, Boolean> getUsers(){
+            return users;
+        }
+
+        public Boolean isTag() {
+            return isTag;
+        }
+
+        public void setTag(Boolean isTag){
+            this.isTag = isTag;
+        }
+
+        public String getEventId(){
+            return eventId;
+        }
+
+        public void setEventId(String eventId){
+            this.eventId = eventId;
+        }
+    }
+
 }
