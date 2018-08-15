2e486b70af7863d52b5ea77262999053b469d84e
Eana Hufwe <ilove@1a23.com>
2018-08-15 16:16:39 +1000

    minor bug fix

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/Social/FriendManager.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/Social/FriendManager.java
@@ -20,6 +20,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.Map;
 
+import au.edu.unimelb.eng.navibee.NaviBeeApplication;
+
 public class FriendManager {
 
     public final static String BROADCAST_FRIEND_UPDATED = "broadcast.friend.updated";
