28d1581b8f17a00665e427172b6e55ab59ca17bd
Shijie Liu <s.liu96@student.unimelb.edu.au>
2018-08-21 15:23:38 +1000

    add chip library

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/EventEditActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/EventEditActivity.java
@@ -1,13 +1,21 @@
 package au.edu.unimelb.eng.navibee;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.ArrayList;
+
 public class EventEditActivity extends AppCompatActivity {
+
+    private ArrayList<String> selectedUidList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.event_edit);
+
+        Intent intent = getIntent();
+        selectedUidList = intent.getStringArrayListExtra("selectedUid");
     }
 }
