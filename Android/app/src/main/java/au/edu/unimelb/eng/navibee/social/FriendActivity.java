b579e2e90f48939f74e8194433dd7173a571e37d
Minjian Chen <m.chen49@student.unimelb.edu.au>
2018-08-22 14:48:20 +1000

    make downloadImageTask a public class

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/FriendActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/FriendActivity.java
@@ -37,6 +37,7 @@ import java.util.Formatter;
 
 import au.edu.unimelb.eng.navibee.NaviBeeApplication;
 import au.edu.unimelb.eng.navibee.R;
+import au.edu.unimelb.eng.navibee.utils.DownloadImageTask;
 
 public class FriendActivity extends AppCompatActivity {
 
@@ -107,74 +108,6 @@ public class FriendActivity extends AppCompatActivity {
             public TextView unread;
             public TextView lastTime;
             public TextView lastMessage;
-        }
-        private class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
-            ImageView bmImage;
-
-            public DownloadImageTask(ImageView bmImage) {
-                this.bmImage = bmImage;
-            }
-
-            protected Bitmap doInBackground(String... urls) {
-                String urldisplay = urls[0];
-                Bitmap mIcon11 = null;
-                String hashedUrl = encryptPassword(urldisplay);
-                String[] fileNames = NaviBeeApplication.getInstance().fileList();
-                if (!Arrays.asList(fileNames).contains(hashedUrl)) {
-                    try {
-                        InputStream in = new java.net.URL(urldisplay).openStream();
-                        mIcon11 = BitmapFactory.decodeStream(in);
-                        File file = new File(NaviBeeApplication.getInstance().getFilesDir(), hashedUrl);
-                        FileOutputStream outputStream = new FileOutputStream(file);
-                        mIcon11.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
-                        outputStream.close();
-                    } catch (Exception e) {
-                        Log.e("Error", e.getMessage());
-                        e.printStackTrace();
-                    }
-                } else {
-                    try {
-                        FileInputStream inputStream = NaviBeeApplication.getInstance().openFileInput(hashedUrl);
-                        mIcon11 = BitmapFactory.decodeStream(inputStream);
-                    } catch (FileNotFoundException e) {
-                    }
-                }
-                return mIcon11;
-            }
-
-            protected void onPostExecute(Bitmap result) {
-                bmImage.setImageBitmap(result);
-            }
-        }
-        private static String encryptPassword(String password){
-            String sha256 = "";
-            try
-            {
-                MessageDigest crypt = MessageDigest.getInstance("SHA-256");
-                crypt.reset();
-                crypt.update(password.getBytes("UTF-8"));
-                sha256 = byteToHex(crypt.digest());
-            }
-            catch(NoSuchAlgorithmException e)
-            {
-                e.printStackTrace();
-            }
-            catch(UnsupportedEncodingException e)
-            {
-                e.printStackTrace();
-            }
-            return sha256;
-        }
-
-        private static String byteToHex(final byte[] hash){
-            Formatter formatter = new Formatter();
-            for (byte b : hash)
-            {
-                formatter.format("%02x", b);
-            }
-            String result = formatter.toString();
-            formatter.close();
-            return result;
         }
     }
 
---
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/utils/DownloadImageTask.java
@@ -0,0 +1,90 @@
+package au.edu.unimelb.eng.navibee.utils;
+
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
+import android.util.Log;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+import java.util.Arrays;
+import java.util.Formatter;
+
+import au.edu.unimelb.eng.navibee.NaviBeeApplication;
+
+public class DownloadImageTask extends AsyncTask<String, Void, Bitmap> {
+    ImageView bmImage;
+
+    public DownloadImageTask(ImageView bmImage) {
+        this.bmImage = bmImage;
+    }
+
+    protected Bitmap doInBackground(String... urls) {
+        String urldisplay = urls[0];
+        Bitmap mIcon11 = null;
+        String hashedUrl = encryptPassword(urldisplay);
+        String[] fileNames = NaviBeeApplication.getInstance().fileList();
+        if (!Arrays.asList(fileNames).contains(hashedUrl)) {
+            try {
+                InputStream in = new java.net.URL(urldisplay).openStream();
+                mIcon11 = BitmapFactory.decodeStream(in);
+                File file = new File(NaviBeeApplication.getInstance().getCacheDir(), hashedUrl);
+                FileOutputStream outputStream = new FileOutputStream(file);
+                mIcon11.compress(Bitmap.CompressFormat.PNG, 90, outputStream);
+                outputStream.close();
+            } catch (Exception e) {
+                Log.e("Error", e.getMessage());
+                e.printStackTrace();
+            }
+        } else {
+            try {
+                FileInputStream inputStream = NaviBeeApplication.getInstance().openFileInput(hashedUrl);
+                mIcon11 = BitmapFactory.decodeStream(inputStream);
+            } catch (FileNotFoundException e) {
+            }
+        }
+        return mIcon11;
+    }
+
+    protected void onPostExecute(Bitmap result) {
+        bmImage.setImageBitmap(result);
+    }
+    private static String encryptPassword(String password){
+        String sha256 = "";
+        try
+        {
+            MessageDigest crypt = MessageDigest.getInstance("SHA-256");
+            crypt.reset();
+            crypt.update(password.getBytes("UTF-8"));
+            sha256 = byteToHex(crypt.digest());
+        }
+        catch(NoSuchAlgorithmException e)
+        {
+            e.printStackTrace();
+        }
+        catch(UnsupportedEncodingException e)
+        {
+            e.printStackTrace();
+        }
+        return sha256;
+    }
+
+    private static String byteToHex(final byte[] hash){
+        Formatter formatter = new Formatter();
+        for (byte b : hash)
+        {
+            formatter.format("%02x", b);
+        }
+        String result = formatter.toString();
+        formatter.close();
+        return result;
+    }
+}
+
