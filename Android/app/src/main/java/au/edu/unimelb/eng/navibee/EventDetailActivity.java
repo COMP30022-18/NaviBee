1618b1528723fd878e072eb1b60f50e95ed73ed6
Mars Xue <marsxue0221@gmail.com>
2018-08-22 21:08:37 +1000

    add join event feature

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/EventDetailActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/EventDetailActivity.java
@@ -3,22 +3,30 @@ package au.edu.unimelb.eng.navibee;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.SetOptions;
 
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Map;
 
 public class EventDetailActivity extends AppCompatActivity {
 
     private FirebaseFirestore db;
+    private String uid;
     private String eid;
+    private Map<String, Boolean> users;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,9 +34,12 @@ public class EventDetailActivity extends AppCompatActivity {
         setContentView(R.layout.event_detail);
 
         db = FirebaseFirestore.getInstance();
+        uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
         eid = getIntent().getStringExtra("eventId");
 
         getEventInfo();
+
+        joinEvent();
 
     }
 
@@ -55,17 +66,64 @@ public class EventDetailActivity extends AppCompatActivity {
                     eventTime.setText(time);
                     eventAddress.setText(address);
 
-                    Map<String, Boolean> docs = ((Map<String, Boolean>) document.getData().get("users"));
+                    users = ((Map<String, Boolean>) document.getData().get("users"));
 
-                    for (Map.Entry<String, Boolean> entry : docs.entrySet()) {
+                    for (Map.Entry<String, Boolean> entry : users.entrySet()) {
                         if (entry.getValue()) {
                             userList.add(entry.getKey());
                         }
                     }
 
+                    // TODO get event participants' name
+
                 } else {
                     // TODO if fails
                 }
+            }
+        });
+    }
+
+    private void joinEvent() {
+
+        Button joinBtn = findViewById(R.id.joinButton);
+
+        joinBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                // reget the user list of event
+                db.collection("events").document(eid).get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            users = ((Map<String, Boolean>) document.getData().get("users"));
+                        } else {
+                            // TODO if fails
+                        }
+                    }
+                });
+
+                // check if the user already join the event
+                for (Map.Entry<String, Boolean> entry : users.entrySet()) {
+                    if (entry.getKey().equals(uid) && entry.getValue().equals(true)) {
+                        Toast.makeText(EventDetailActivity.this,
+                                "Already join the event", Toast.LENGTH_LONG).show();
+                        return;
+                    }
+                }
+
+                Map<String, Object> data = new HashMap<>();
+                Map<String, Boolean> newData = new HashMap<>();
+
+                newData.put(uid, true);
+
+                data.put("users", newData);
+                db.collection("events").document(eid).set(data, SetOptions.merge());
+
+                Toast.makeText(EventDetailActivity.this,
+                        "Successfully join the event", Toast.LENGTH_LONG).show();
+
             }
         });
     }
