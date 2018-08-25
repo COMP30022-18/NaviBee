b57841b4d5885a3019f305b3908d05b37b9a7df1
CaviarChen <yzj@vlabpro.com>
2018-08-25 23:00:09 +1000

    [backend] add newMessageNotification to trigger notification

--- Backend/functions/src/helper/admin.ts
+++ Backend/functions/src/helper/admin.ts
@@ -1,5 +1,5 @@
 import { config } from 'firebase-functions';
-import { initializeApp, firestore } from 'firebase-admin';
+import { initializeApp, firestore, messaging } from 'firebase-admin';
 
 initializeApp(config().firebase);
 
@@ -9,3 +9,5 @@ const fs = firestore();
 fs.settings(settings);
 
 export const db = fs;
+export const msg = messaging();
+export const msgType = messaging;
