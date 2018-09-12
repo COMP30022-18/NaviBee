d2db268895801742bb73d4d7bcf2346baf46b095
CaviarChen <yzj@vlabpro.com>
2018-09-12 22:03:32 +1000

    change backend to fit new db structure

--- Backend/functions/src/main/user.ts
+++ Backend/functions/src/main/user.ts
@@ -7,25 +7,9 @@ export const initNewUser = functions.auth.user().onCreate((user) => {
         name: user.displayName,
         email: user.email,
         photoURL: user.photoURL,
-        contacts: {}
     };
 
     let userRef = db.collection('users').doc(user.uid); // user doc reference
     userRef.set(data);
     return 0;
 });
-
-
-export const getUserInfoFromUidList = functions.https.onCall(
-    async (data, context) => {
-        const result = {};
-
-        for (let uid of data.uidList) {
-            let userDoc = (await db.collection('users').doc(uid).get()).data();
-            result[uid] = {};
-            result[uid]['name'] = userDoc.name;
-            result[uid]['photoURL'] = userDoc.photoURL;
-        }
-
-        return result;
-    });
