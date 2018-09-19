0372fc6ca0985e94dc015cf65a5e92f901d2d823
Minjian Chen <minjianc@student.unimelb.edu.au>
2018-09-19 15:06:13 +1000

    adjust friendDetail, createGroupChat layout

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/FriendDetail.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/FriendDetail.java
@@ -3,9 +3,12 @@ package au.edu.unimelb.eng.navibee.social;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import au.edu.unimelb.eng.navibee.R;
+import au.edu.unimelb.eng.navibee.utils.NetworkImageHelper;
 
 public class FriendDetail extends AppCompatActivity {
     private String convId;
@@ -17,6 +20,13 @@ public class FriendDetail extends AppCompatActivity {
         setContentView(R.layout.activity_friend_detail);
         convId = getIntent().getStringExtra("CONV_ID");
         friendId = getIntent().getStringExtra("FRIEND_ID");
+        ImageView icon = findViewById(R.id.friend_detail_icon);
+        TextView name = findViewById(R.id.friend_detail_name);
+        UserInfoManager.getInstance().getUserInfo(friendId, userInfo -> {
+            name.setText(userInfo.getName());
+            NetworkImageHelper.loadImage(icon, userInfo.getHighResolutionPhotoUrl());
+        });
+
     }
     protected void onClick(View view){
         switch (view.getId()) {
@@ -24,6 +34,8 @@ public class FriendDetail extends AppCompatActivity {
                 Intent intent = new Intent(getBaseContext(), ChatActivity.class);
                 intent.putExtra("CONV_ID", convId);
                 startActivity(intent);
+                break;
+            case R.id.activity_friend_detail_deleteFriend_button:
                 break;
         }
     }
