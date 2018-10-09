99cd3373e0a68d7dcd668d8e084dfa505685e1cf
Mars Xue <marsxue0221@gmail.com>
2018-10-09 17:16:41 +1100

    add floating window for voice call

---
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/OnFlingListener.java
@@ -0,0 +1,21 @@
+package au.edu.unimelb.eng.navibee.social;
+
+/**
+ * 滑动手势抽象类
+ */
+public abstract class OnFlingListener {
+    public void onSlideUp() {
+    }
+
+    public void onSlideDown() {
+    }
+
+    public void onSlideLeft() {
+    }
+
+    public void onSlideRight() {
+    }
+
+    public void onMove(float moveX, float moveY) {
+    }
+}
