72c5f2048dfa6248ab01849c2270bc15a3c4585a
CaviarChen <yzj@vlabpro.com>
2018-10-10 15:33:33 +1100

    update LocationDisplayActivity

--- Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/LocationDisplayActivity.java
+++ Android/app/src/main/java/au/edu/unimelb/eng/navibee/social/LocationDisplayActivity.java
@@ -4,7 +4,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import au.edu.unimelb.eng.navibee.R;
 import au.edu.unimelb.eng.navibee.navigation.NavigationSelectorActivity;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.view.View;
 
@@ -19,9 +21,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 public class LocationDisplayActivity extends AppCompatActivity implements OnMapReadyCallback {
 
     private static final float DEFAULT_ZOOM_LEVEL = 15.0f;
+    private static final int PERMISSIONS_REQUEST_FINE_LOCATION = 1;
 
     private double lat, lon;
     private MapView mapView;
+    private GoogleMap googleMap = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -35,6 +39,35 @@ public class LocationDisplayActivity extends AppCompatActivity implements OnMapR
         mapView.onCreate(savedInstanceState);
         mapView.getMapAsync(this);
 
+        if (checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, PERMISSIONS_REQUEST_FINE_LOCATION);
+        }
+
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        switch (requestCode) {
+            case PERMISSIONS_REQUEST_FINE_LOCATION: {
+                // If request is cancelled, the result arrays are empty.
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+
+                    if (googleMap!=null) {
+                        googleMap.setMyLocationEnabled(true);
+                    }
+
+                }
+                return;
+            }
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
     }
 
     @Override
@@ -44,9 +77,34 @@ public class LocationDisplayActivity extends AppCompatActivity implements OnMapR
     }
 
     @Override
+    protected void onStop() {
+        super.onStop();
+        mapView.onStop();
+    }
+    @Override
+    protected void onPause() {
+        mapView.onPause();
+        super.onPause();
+    }
+    @Override
+    protected void onDestroy() {
+        mapView.onDestroy();
+        super.onDestroy();
+    }
+    @Override
+    public void onLowMemory() {
+        super.onLowMemory();
+        mapView.onLowMemory();
+    }
+
+    @Override
     public void onMapReady(GoogleMap googleMap) {
+        this.googleMap = googleMap;
         LatLng location = new LatLng(lat, lon);
-        googleMap.setMyLocationEnabled(true);
+        if (!(checkSelfPermission(Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && checkSelfPermission(Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)) {
+            googleMap.setMyLocationEnabled(true);
+        }
+
         googleMap.addMarker(new MarkerOptions().position(location));
         googleMap.moveCamera(CameraUpdateFactory.newLatLng(location));
         googleMap.moveCamera(CameraUpdateFactory.zoomTo(DEFAULT_ZOOM_LEVEL));
